29f020564588dcbac5e1ed242466ff2b72e501db
Dicky <run26kimo@gmail.com>
2011-07-14 12:37:03 +0800

    job 3 finish

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  
+  before_filter :authenticate_user! , :except => [ :show, :index ]
+  
   # GET /posts
   # GET /posts.json
   def index
@@ -32,7 +35,7 @@ class PostsController < ApplicationController
 
   # GET /posts/1/edit
   def edit
-    @post = Post.find(params[:id])
+    @post = current_user.posts.find(params[:id])
   end
 
   # POST /posts
@@ -40,6 +43,7 @@ class PostsController < ApplicationController
   def create
     @board = Board.find(params[:board_id])
     @post = @board.posts.build(params[:post])
+    @post.user_id = current_user.id
     
     respond_to do |format|
       if @post.save
@@ -56,7 +60,7 @@ class PostsController < ApplicationController
   # PUT /posts/1.json
   def update
     @board = Board.find(params[:board_id])
-    @post = @board.posts.find(params[:id])
+    @post = current_user.posts.find(params[:id])
     
     respond_to do |format|
       if @post.update_attributes(params[:post])
@@ -73,7 +77,8 @@ class PostsController < ApplicationController
   # DELETE /posts/1.json
   def destroy
     @board = Board.find(params[:board_id])
-    @post = @board.posts.find(params[:id])
+    #@post = @board.posts.find(params[:id])
+    @post = current_user.posts.find(params[:id])
     @post.destroy
 
     respond_to do |format|
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Fb::Application.routes.draw do
-    
+      
+    devise_for :users
+
     resources :boards do
     resources :posts
   end
 
-  root :to => "boards#index"
+  namespace :admin do 
+    resources :boards do
+      resources :posts 
+    end
+  end
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -56,10 +62,11 @@ Fb::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+root :to => "boards#index"
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-   match ':controller(/:action(/:id(.:format)))'
+  match ':controller(/:action(/:id(.:format)))'
 end
